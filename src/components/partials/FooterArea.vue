e410e60f642ae6d240c40dab3c4b4c45f32322bd
Joaquin (Pato) Decima <jdecima@vasak.net.ar>
2024-11-22 20:48:18 -0300

    agregar contenido y estilos al componente FooterArea

--- src/components/partials/FooterArea.vue
+++ src/components/partials/FooterArea.vue
@@ -1,3 +1,10 @@
 <template>
-  <footer></footer>
+    <footer
+      class="p-28 mt-3 text-slate-700 dark:text-gray-400 bg-white dark:bg-gray-900 shadow-lg font-medium"
+    >
+      <p class="text-center common-text">
+        Powered by
+        <a href="https://vasak.net.ar/" target="_blank">Vasak Group</a>
+      </p>
+    </footer>
 </template>
