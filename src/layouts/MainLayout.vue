268d425b8d25d72360f4a29b2fa9b6d81d7917c1
Joaquin (Pato) Decima <jdecima@vasak.net.ar>
2024-11-22 20:28:37 -0300

    agregar componentes FooterArea y NavBar, y actualizar MainLayout para incluirlos

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import FooterArea from '../components/partials/FooterArea.vue';
+import NavBar from '../components/partials/NavBar.vue';
+</script>
+
+<template>
+  <NavBar />
+  <slot />
+  <FooterArea />
+</template>
