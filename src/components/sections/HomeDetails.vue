0df038f2189140100a7caa03e01e89120653b1cf
Joaquin (Pato) Decima <jdecima@vasak.net.ar>
2024-11-23 12:02:05 -0300

    agregar nuevas secciones HomeDetails y HomeStart, y actualizar HomePage para incluirlas

---
+++ src/components/sections/HomeDetails.vue
@@ -0,0 +1,35 @@
+<template>
+  <section class="mx-auto w-full px-8 max-w-5xl">
+    <div class="py-12 md:py-24">
+      <div class="grid gap-12 grid-cols-1 md:grid-cols-2">
+        <div class="flex flex-col !space-y-4 justify-center">
+          <h3 class="font-display font-bold text-typo m-0 text-4xl">
+            Quita fondos de manera automática en solo cinco segundos
+          </h3>
+          <div class="text-typo text-base">
+            <p>
+              ¡Aprovecha RemoveBG para ahorrar tiempo de edición y divertirte!
+            </p>
+            <p>
+              <span class=""
+                >No importa si quieres un fondo transparente (PNG) o agregar un
+                fondo blanco a tu imagen, puedes hacer esto y más a partir de RemoveBG
+              </span>
+            </p>
+          </div>
+        </div>
+        <div class="flex flex-col space-y-4 justify-center">
+          <figure class="!m-0">
+            <picture
+              ><img
+                src="https://sb.kaleidousercontent.com/67418/658x756/361846cee7/all-pages-2.png"
+                class="w-full h-auto rounded-2xl md:rounded-4xl mx-auto max-w-[66%]"
+                loading="lazy"
+                draggable="false" /></picture
+            ><!---->
+          </figure>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,27 +1,14 @@
 <script setup lang="ts">
-import demoHome from "../assets/img/demo_home.png"
+import demoHome from "../assets/img/demo_home.png";
 import FileDrop from "../components/FileDrop.vue";
+import HomeDetails from "../components/sections/HomeDetails.vue";
+import HomeStart from "../components/sections/HomeStart.vue";
 </script>
 
 <template>
   <main>
-    <section class="p-36 w-full flex flex-wrap justify-center items-center">
-      <div class="p-5 flex flex-col justify-center me-8">
-        <img :src="demoHome" alt="Demo Home" class="max-w-96 mb-6"/>
-        <p class="my-auto mb-12">
-          <span class="text-6xl font-bold">
-            Quita el Fondo <br />
-            a las imágenes
-          <br/>
-          </span>
-          <br/>
-          <span class="text-xl font-medium mt-12">
-            100% Gratis y automático
-          </span>
-        </p>
-      </div>
-      <FileDrop />
-    </section>
+    <HomeStart />
     <div id="#download" class="display-none"></div>
+    <HomeDetails />
   </main>
 </template>
