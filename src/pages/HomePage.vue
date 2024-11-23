affeb204d393ece7f3e7104c74c2e850ca796ac9
Joaquin (Pato) Decima <jdecima@vasak.net.ar>
2024-11-23 10:42:28 -0300

    mejorar diseño de HomePage con nueva imagen y texto promocional

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,19 +1,27 @@
 <script setup lang="ts">
-import FileDrop from '../components/FileDrop.vue';
+import demoHome from "../assets/img/demo_home.png"
+import FileDrop from "../components/FileDrop.vue";
 </script>
 
 <template>
-  <main class="p-56 w-full flex flex-col justify-center items-center">
-    <p class="my-auto text-center mb-12">
-      <span class="text-3xl font-bold">
-        Elimina el Fondo Instantaneamente
-      </span>
-      <br />
-      <span class="text-xl font-medium">
-        Remueve el fondo de tus fotos en segundos
-      </span>
-    </p>
-    <FileDrop />
+  <main>
+    <section class="p-36 w-full flex flex-wrap justify-center items-center">
+      <div class="p-5 flex flex-col justify-center me-8">
+        <img :src="demoHome" alt="Demo Home" class="max-w-96 mb-6"/>
+        <p class="my-auto mb-12">
+          <span class="text-6xl font-bold">
+            Quita el Fondo <br />
+            a las imágenes
+          <br/>
+          </span>
+          <br/>
+          <span class="text-xl font-medium mt-12">
+            100% Gratis y automático
+          </span>
+        </p>
+      </div>
+      <FileDrop />
+    </section>
     <div id="#download" class="display-none"></div>
   </main>
 </template>
