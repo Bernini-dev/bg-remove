64fa51ed44a50b8c078a20374ef80755f57f8a65
Joaquin (Pato) Decima <jdecima@vasak.net.ar>
2024-11-23 14:37:02 -0300

    agregar el componente PriceDetails a HomePage para mostrar información de precios

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,8 +1,7 @@
 <script setup lang="ts">
-import demoHome from "../assets/img/demo_home.png";
-import FileDrop from "../components/FileDrop.vue";
 import HomeDetails from "../components/sections/HomeDetails.vue";
 import HomeStart from "../components/sections/HomeStart.vue";
+import PriceDetails from "../components/sections/PriceDetails.vue";
 </script>
 
 <template>
@@ -10,5 +9,6 @@ import HomeStart from "../components/sections/HomeStart.vue";
     <HomeStart />
     <div id="#download" class="display-none"></div>
     <HomeDetails />
+    <PriceDetails />
   </main>
 </template>
