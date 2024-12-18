a2cd7d3249c19bb9eb1345b73ebe19c366b622b2
Joaquin (Pato) Decima <jdecima@vasak.net.ar>
2024-12-18 19:06:24 -0300

    eliminar componentes NavBar y FooterArea del diseño principal para simplificar la estructura

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,10 +1,6 @@
 <script setup lang="ts">
-import FooterArea from '../components/partials/FooterArea.vue';
-import NavBar from '../components/partials/NavBar.vue';
 </script>
 
 <template>
-  <NavBar />
   <slot />
-  <FooterArea />
 </template>
