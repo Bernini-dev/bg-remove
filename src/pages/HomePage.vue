9395b98037be82978043924d2f2cf05f3da13962
Joaquin (Pato) Decima <jdecima@vasak.net.ar>
2024-12-18 19:06:50 -0300

    eliminar componentes HomeDetails y PriceDetails de HomePage para simplificar la estructura

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,14 +1,10 @@
 <script setup lang="ts">
-import HomeDetails from "../components/sections/HomeDetails.vue";
 import HomeStart from "../components/sections/HomeStart.vue";
-import PriceDetails from "../components/sections/PriceDetails.vue";
 </script>
 
 <template>
   <main>
     <HomeStart />
     <div id="#download" class="display-none"></div>
-    <HomeDetails />
-    <PriceDetails />
   </main>
 </template>
