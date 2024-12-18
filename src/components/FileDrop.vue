26e56f793c458947a952ffffc17d24ad82fbf757
Joaquin (Pato) Decima <jdecima@vasak.net.ar>
2024-12-18 19:56:41 -0300

    refactor: simplificar la gestión de archivos en FileDrop.vue

--- src/components/FileDrop.vue
+++ src/components/FileDrop.vue
@@ -1,27 +1,19 @@
 <script lang="ts" setup>
 import { Ref, ref } from "vue";
-import { removeBackground } from "@imgly/background-removal";
-import { useLoadingStore } from "../stores/loadingStore";
 
-const allFiles: Ref<File[]> = ref([]);
+const props = defineProps(["allFiles"]);
+const emits = defineEmits(["update:allFiles"]);
 const isDragover: Ref<Boolean> = ref(false);
-const loadingStore = useLoadingStore();
 
 const dropHandler = (event: any) => {
   const droppedFiles = Array.from(event.dataTransfer.files);
-  console.log(droppedFiles);
-  droppedFiles.forEach((f: any) => {
-    allFiles.value.push(f);
-  });
+  emits("update:allFiles", [...props.allFiles, ...droppedFiles]);
   isDragover.value = false;
 };
 
 const addImage = (event: any) => {
   const droppedFiles = Array.from(event.target.files);
-  console.log(droppedFiles);
-  droppedFiles.forEach((f: any) => {
-    allFiles.value.push(f);
-  });
+  emits("update:allFiles", [...props.allFiles, ...droppedFiles]);
 };
 
 const setIsDragOver = (status: boolean) => {
@@ -32,29 +24,12 @@ const chooseFiles = () => {
   document.getElementById("imageUpload")?.click();
 };
 
-const deleteFile = (file: File) => {
-  allFiles.value = allFiles.value.filter((f) => f !== file);
-};
-
-const removeBackgroundFile = async (file: File) => {
-  loadingStore.startLoading();
-  await removeBackground(file).then((result: any) => {
-    const url = window.URL.createObjectURL(result);
-    var a = document.createElement("a");
-    document.getElementById("#download")?.appendChild(a);
-    a.href = url;
-    a.download = file.name;
-    a.click();
-    window.URL.revokeObjectURL(url);
-  });
-  loadingStore.endLoading();
-};
 </script>
 
 <template>
   <div
     class="border-2 w-full flex flex-col justify-center items-center gap-8 pt-16 pb-16 rounded-3xl shadow-2xl"
-    style="background: #201E33;"
+    style="background: #201e33"
     @drop.prevent="dropHandler($event)"
     @dragover.prevent="setIsDragOver(true)"
     @dragleave.prevent="setIsDragOver(false)"
@@ -69,38 +44,21 @@ const removeBackgroundFile = async (file: File) => {
         <div
           class="[&amp;>svg]:scale-133 [&amp;>svg]:shrink-0 flex items-center gap-2"
         >
-        <font-awesome-icon
-          icon="fas fa-cloud-upload-alt"
-          class="text-2xl text-white ms-1"
-        />
+          <font-awesome-icon
+            icon="fas fa-cloud-upload-alt"
+            class="text-2xl text-white ms-1"
+          />
           Cargar imagen
         </div>
       </button>
-      <input id="imageUpload" ref="file" type="file" hidden @change="addImage($event)" />
-      <p class="m-0 font-bold text-xl">
-        O arrastrala y sueltala aquí
-      </p>
-    </div>
-    <div
-      v-for="f in allFiles"
-      :key="f.name"
-      class="w-10/12 px-5 py-1 my-1 flex justify-center border rounded-xl"
-    >
-      <div>{{ f.name }}</div>
-      <div class="ms-auto">
-        <span class="p-2" @click="removeBackgroundFile(f)">
-          <font-awesome-icon
-            icon="fas fa-cloud-download-alt"
-            class="text-slate-300"
-          />
-        </span>
-        <span class="p-2" @click="deleteFile(f)">
-          <font-awesome-icon
-            icon="fas fa-xmark"
-            class="text-slate-300"
-          />
-        </span>
-      </div>
+      <input
+        id="imageUpload"
+        ref="file"
+        type="file"
+        hidden
+        @change="addImage($event)"
+      />
+      <p class="m-0 font-bold text-xl">O arrastrala y sueltala aquí</p>
     </div>
   </div>
 </template>
