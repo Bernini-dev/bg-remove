49d7842719be2da4e930523ef9ed3d5753357167
Joaquin (Pato) Decima <jdecima@vasak.net.ar>
2024-12-18 21:09:56 -0300

    refactor: eliminar LoadingComponent y simplificar la gestión de carga en App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,9 @@
 <script setup lang="ts">
-import LoadingComponent from './components/LoadingComponent.vue';
 import MainLayout from './layouts/MainLayout.vue';
 import HomePage from './pages/HomePage.vue';
-import { useLoadingStore } from './stores/loadingStore';
-
-const loadingStore = useLoadingStore();
 </script>
 
 <template>
-  <LoadingComponent v-if="loadingStore.loading" />
   <MainLayout>
     <HomePage />
   </MainLayout>
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,4 @@
 import { createApp } from "vue";
-import { createPinia } from "pinia";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 import {
@@ -15,7 +14,6 @@ import "./style.css";
 import App from "./App.vue";
 
 const app = createApp(App);
-const pinia = createPinia();
 
 library.add(
   faXmark,
@@ -27,6 +25,5 @@ library.add(
 );
 
 app.component("font-awesome-icon", FontAwesomeIcon);
-app.use(pinia);
 
 app.mount("#app");
