a9a9b3944027f6df2000c63b0709422c37ba1f98
Joaquin (Pato) Decima <jdecima@vasak.net.ar>
2024-12-19 13:35:56 -0300

    refactor: mejorar el diseño y la gestión del componente FileConvert

--- src/components/FileConvert.vue
+++ src/components/FileConvert.vue
@@ -5,6 +5,7 @@ import { removeBackground } from "@imgly/background-removal";
 const props = defineProps(["file"]);
 const emit = defineEmits(["deleteFile"]);
 const loading: Ref<boolean> = ref(false);
+const mainWindow = window;
 
 const deleteFile = () => {
   emit("deleteFile");
@@ -26,9 +27,38 @@ const removeBackgroundFile = async () => {
 </script>
 
 <template>
-  <div class="p-3 rounded-xl flex" style="background: #201e33">
+  <div
+    class="p-3 rounded-xl flex flex-col gap-2 w-80 relative pt-8"
+    style="background: #201e33"
+  >
+    <div
+      class="rounded-xl bg-center bg-no-repeat bg-cover aspect-video w-full mt-2 relative"
+      :style="`background-image: url(${mainWindow.URL.createObjectURL(
+        props.file
+      )});`"
+    >
+      <div
+        v-if="loading"
+        class="absolute top-0 bottom-0 left-0 right-0 rounded-xl flex flex-wrap justify-center content-center"
+        style="background: #191135eb"
+      >
+        <div
+          class="inline-block h-8 w-8 animate-spin rounded-full border-4 border-solid border-current border-e-transparent align-[-0.125em] text-surface motion-reduce:animate-[spin_1.5s_linear_infinite] dark:text-white"
+          role="status"
+        >
+          <span
+            class="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]"
+            >Loading...</span
+          >
+        </div>
+      </div>
+    </div>
+
     {{ props.file.name }}
-    <button class="bg-brand text-white" @click="removeBackgroundFile()">
+    <button
+      class="bg-brand text-white rounded-xl p-3"
+      @click="removeBackgroundFile()"
+    >
       Descargar
       <font-awesome-icon
         icon="fas fa-cloud-download-alt"
@@ -36,8 +66,9 @@ const removeBackgroundFile = async () => {
       />
     </button>
     <div class="hidde"></div>
+
+    <span class="p-2 absolute top-1 right-1" @click="deleteFile()">
+      <font-awesome-icon icon="fas fa-xmark" class="text-slate-300" />
+    </span>
   </div>
-  <span class="p-2" @click="deleteFile()">
-    <font-awesome-icon icon="fas fa-xmark" class="text-slate-300" />
-  </span>
 </template>
